462ad9df4b73d4b8c9f7b5eb5a35376fca666cde
Kami <fengkehan@bosc.ac.cn>
2025-05-08 14:59:53 +0800

    timing: A three-stage state machine is used to optimize the timing and remove unnecessary state operations

--- sources/axis_data_packge.v
+++ sources/axis_data_packge.v
@@ -22,6 +22,11 @@ module axis_data_packge #(
   input  [DATA_WIDTH-1:0] data
 );
     localparam AXIS_SEND_LEN = ((DATA_WIDTH + AXIS_DATA_WIDTH + 8 - 1) / AXIS_DATA_WIDTH) - 1;
+    localparam IDLE = 3'b001;
+    localparam TRANSFER = 3'b010;
+    localparam DONE = 3'b100;
+
+    reg [2:0] current_state, next_state;
 
     reg [DATA_WIDTH - AXIS_DATA_WIDTH + 8 - 1:0] mix_data;
     reg [AXIS_DATA_WIDTH - 1:0]  reg_m_axis_c2h_tdata;
@@ -48,11 +53,14 @@ module axis_data_packge #(
     assign m_axis_c2h_tkeep = 64'hffffffff_ffffffff;
     assign m_axis_c2h_tlast = reg_m_axis_c2h_tlast;
 
+    wire both_full = buffer_0_valid & buffer_1_valid;
+    wire any_valid_and_data = (buffer_0_valid | buffer_1_valid) & data_valid;
+
     always @(posedge m_axis_c2h_aclk) begin
         if (!rstn) begin
-            reg_data_next <= 1;
+            reg_data_next <= 1'b1;
         end else begin
-            reg_data_next <= !(buffer_0_valid & buffer_1_valid) & !((buffer_0_valid || buffer_1_valid) & data_valid);
+            reg_data_next <= ~both_full & ~any_valid_and_data;
         end
     end
 
@@ -74,9 +82,27 @@ module axis_data_packge #(
     wire core_data_sampling_en = data_valid;
 `endif // ASYN_SEND_DATA
 
+    wire can_send = (this_buffer ? buffer_0_valid : buffer_1_valid);
+
+    always @(posedge m_axis_c2h_aclk) begin
+        if (!m_axis_c2h_aresetn || !rstn)
+            current_state <= IDLE;
+        else
+            current_state <= next_state;
+    end
+
+    always @(*) begin
+        case(current_state)
+            IDLE:     next_state = can_send ? TRANSFER : IDLE;
+            TRANSFER: next_state = (m_axis_c2h_tready && reg_m_axis_c2h_tvalid) ?
+                                   (datalen == AXIS_SEND_LEN ? DONE : TRANSFER) : TRANSFER;
+            DONE:     next_state = IDLE;
+            default:  next_state = IDLE;
+        endcase
+    end
+
     always @(posedge m_axis_c2h_aclk) begin
         if(!m_axis_c2h_aresetn || !rstn) begin
-            state <= 0;
             reg_m_axis_c2h_tvalid <= 0;
             reg_m_axis_c2h_tlast <= 0;
             datalen <= 0;
@@ -98,51 +124,40 @@ module axis_data_packge #(
                 current_buffer <= ~current_buffer; // Switch buffers
             end
 
-            case(state)
-            0 : begin
-                if ((this_buffer & buffer_0_valid) || (!this_buffer & buffer_1_valid)) begin
-                    if (this_buffer == 1 && buffer_0_valid) begin
+            case(current_state)
+            IDLE : begin
+                if (can_send) begin
+                    if (this_buffer == 1) begin
                         reg_m_axis_c2h_tdata <= {buffer_0[AXIS_DATA_WIDTH - 8 - 1:0], data_num};
                         mix_data <= buffer_0[DATA_WIDTH - 1:AXIS_DATA_WIDTH - 8];
                         buffer_0_valid <= 0;
-                    end else if (this_buffer == 0 && buffer_1_valid) begin
+                    end else begin
                         reg_m_axis_c2h_tdata <= {buffer_1[AXIS_DATA_WIDTH - 8 - 1:0], data_num};
                         mix_data <= buffer_1[DATA_WIDTH - 1:AXIS_DATA_WIDTH - 8];
                         buffer_1_valid <= 0;
                     end
                     reg_m_axis_c2h_tvalid <= 1;
-                    state <= 1;
-                    datalen <= 0;
                     data_num <= data_num + 1'b1;
                     this_buffer <= ~this_buffer;
-                end else begin
-                    state <= 0;
-                    datalen <= 0;
                 end
             end 
-            1: begin
+            TRANSFER: begin
                 if(m_axis_c2h_tready && reg_m_axis_c2h_tvalid) begin
                     reg_m_axis_c2h_tdata <= mix_data[AXIS_DATA_WIDTH - 1:0];
                     mix_data <= mix_data >> AXIS_DATA_WIDTH;
                     if(datalen == (AXIS_SEND_LEN - 1)) begin
                         reg_m_axis_c2h_tlast <= 1;
-                        state <= 1;
                     end else if(datalen == AXIS_SEND_LEN) begin
-                        state <= 2;
                         reg_m_axis_c2h_tlast <= 0;
                         reg_m_axis_c2h_tvalid <= 0;
-                    end else begin
-                        state <= 1;
                     end
                     datalen <= datalen + 1'b1;
-                end else begin
-                    state <= 1;
-                end   
+                end
             end
-            2: begin
+            DONE: begin
                 reg_m_axis_c2h_tvalid <= 0;
                 reg_m_axis_c2h_tlast <= 0;
-                state <= 0;
+                datalen <= 0;
             end
             endcase
         end
